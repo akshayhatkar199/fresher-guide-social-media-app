85127d43739cb22fd90288194c9bd5aab0c7ceca
Akhsay Hatkar <akshayhatkar99@gmail.com>
2022-11-30 19:43:08 +0530

    login page design done

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,16 +1,131 @@
 import React from 'react'
 import { Button} from 'antd';
 import {Link } from "react-router-dom";
+import { Col, Row } from 'antd';
+import { Card } from 'antd';
+import {  Checkbox, Form, Input } from 'antd';
+import Image from '../../images/logo-login.png';
+import "./login.css";
+
 
 const Login = () => {
+
+    const onFinish = (values) => {
+        console.log('Success:', values);
+      };
+      const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+      };
+
   return (
-    <>
-    <h1>Login</h1>
-    <br/>
-    <Link to= "/home"> <Button type="primary">Home Button</Button> </Link>
+    <div className='logincontainer'>
+
+    <Row>
+    <Col
+
+      xs={{span: 24}}
+      sm={{span: 24}}
+      md={{span: 6}}
+      lg={{span: 6 }}
+      xl={{span: 8}}
+      xxl={{span: 8}}
+    >
+      
+
+    </Col>
+    <Col
+     xs={{span: 24}}
+      sm={{span: 24}}
+      md={{span: 12}}
+      lg={{span: 12 }}
+      xl={{span: 8}}
+      xxl={{span: 8}}
+    
+    >
+    
+     <Card
+    className='login-card'
+      title=" "
+      bordered={false}
+           
+    >
+      <img src= {Image} className =" login-logo"/>
      
-    <Link to= "/registration"> <Button type="primary">Registration Button</Button></Link>
-</>
+     
+     
+       <Form
+       layout="vertical"
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+     
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Email"
+        name="email"
+        
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Email!',
+          },
+        ]}
+      >
+        <Input style={{  width: "100%"}} />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        // wrapperCol={{
+        //   offset: 5,
+        //   span: 16,
+        // }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+
+        <Link to= "/registration"> <Button type="link">Registration</Button></Link>
+      </Form.Item>
+    </Form>
+
+
+    </Card>
+    </Col>
+
+    <Col
+     xs={{span: 24}}
+      sm={{span: 24}}
+      md={{span: 6}}
+      lg={{span: 6 }}
+      xl={{span: 8}}
+      xxl={{span: 8}}
+   
+    >
+    
+    </Col>
+
+  </Row>
+</div>
   )
 }
 
