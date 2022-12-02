0bd419ac7e3f0209ccdd982ffc7e305bf335b8d5
Akhsay Hatkar <akshayhatkar99@gmail.com>
2022-12-02 18:36:57 +0530

    card post done

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,12 +2,77 @@
 import React from 'react'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
+import { Col, Row  } from 'antd';
+import Postcard from "../../components/Postcard"
 import './Home.css'
 
 const Home = () => {
   return (
     <div> 
     <Header />
+    <div className='main-home-container'>
+    <Row>
+
+      <Col  
+      xs={{span: 24}}
+      sm={{span: 24}}
+      md={{span: 24}}
+      lg={{span: 4}}
+      xl={{span: 4}}
+      xxl={{span: 4}}
+      >
+      <div className='home-container-1'>
+
+      152454
+      
+      </div>
+      </Col>
+
+      <Col  
+      xs={{span: 24}}
+      sm={{span: 24}}
+      md={{span: 24}}
+      lg={{span: 14}}
+      xl={{span: 14}}
+      xxl={{span: 14}}
+      >
+      <div className='home-container-2'>
+      <Postcard/>
+      <Postcard/>
+      <Postcard/>
+      <Postcard/>
+      <Postcard/>
+     
+
+
+
+
+
+
+
+
+
+
+      </div>
+      </Col>
+
+      <Col  
+      xs={{span: 24}}
+      sm={{span: 24}}
+      md={{span: 24}}
+      lg={{span: 6}}
+      xl={{span: 6}}
+      xxl={{span: 6}}
+      >
+      <div className='home-container-3' >
+      41111
+
+      </div>
+      </Col>
+    
+    </Row>
+
+    </div>
     <Footer />
     </div>
   )
---
+++ src/components/Postcard/index.js
@@ -0,0 +1,90 @@
+
+import React from 'react'
+import { Col, Row,  } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faComment, faThumbsUp} from '@fortawesome/free-solid-svg-icons'
+import Image1 from '../../images/user.jpg';
+import Image2 from '../../images/post-image.jpg';
+
+import { Card } from 'antd';
+
+const Postcard = () => {
+  return (
+    <div> 
+      <Card
+    className='post-card'
+  >
+  <div className='post'>
+    <Row>
+      <Col  
+      xs={{span: 7}}
+      sm={{span: 5}}
+      md={{span: 4}}
+      lg={{span: 4}}
+      xl={{span:3 }}
+      xxl={{span:3}}
+      >
+        <img src={Image1} alt="logo" className="post-image "></img> 
+
+      </Col>
+
+      <Col  
+      xs={{span: 17 }}
+      sm={{span: 19}}
+      md={{span: 20}}
+      lg={{span: 20 }}
+      xl={{span: 21}}
+      xxl={{span:21}}
+      >
+        <h3 className='post-name'> Sufiya Eliza <br />
+        <span className='post-time'> 30 Mins Ago</span>
+        </h3>
+        
+
+      </Col>
+    </Row>
+    </div>
+  <hr/>
+
+  <img src={Image2} alt="logo" className="user-post"></img> 
+  <div className='post-desc'>
+    <h4>Today Our Three Cute Puppy Dog Birthday !!!!</h4>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. has been the industry's standard dummy text ever since the 1500s</p>
+  </div>
+  <hr/>
+<div className='like-comment'>
+      <Row>
+      <Col
+            xs={{span: 12}}
+            sm={{span: 12}}
+            md={{span: 12}}
+            lg={{span: 12}}
+            xl={{span: 12}}
+            xxl={{span: 12}}
+      >
+      <FontAwesomeIcon icon={faThumbsUp} className="card-like"/>Like
+      </Col>
+
+        <Col
+            xs={{span: 12}}
+            sm={{span: 12}}
+            md={{span: 12}}
+            lg={{span: 12}}
+            xl={{span: 12}}
+            xxl={{span: 12}}
+      >
+      <FontAwesomeIcon icon={ faComment} className="card-comment"/> 
+      Coment
+
+      </Col>
+
+      </Row>
+
+</div>
+
+  </Card> <br/>
+      </div>
+  )
+}
+
+export default Postcard
